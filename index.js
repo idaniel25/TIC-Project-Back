98b1e5e61c8f6a82c2f8d1443961ec4579e9d19a
danutz <iovanadaniel3@gmail.com>
2023-11-30 19:38:46 +0200

    add nodemon and test server

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const admin = require('firebase-admin');
 
 // Configurare Firebase Admin SDK
-const serviceAccount = require('tic-project-9df42-firebase-adminsdk-l42qn-f7d45d0820.json'); // Adaugă calea corectă către cheile de autentificare
+const serviceAccount = require('./tic-project-9df42-firebase-adminsdk-l42qn-f7d45d0820.json'); // Adaugă calea corectă către cheile de autentificare
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
@@ -13,6 +13,11 @@ const db = admin.firestore();
 const app = express();
 
 // Definirea rutelor și logicilor de server continuă aici...
+
+// Ruta pentru un mesaj de testare
+app.get('/', (req, res) => {
+    res.send('Serverul backend rulează cu succes!');
+  });
 
 // Ascultă pe un anumit port (de exemplu, 3000)
 const port = process.env.PORT || 3000;
